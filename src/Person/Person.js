13edf32b9f01ba127cff473f86a93bb5316b9f82
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-26 18:08:18 -0400

    Added my own styling.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Person.css';
 
 const person = props => {
+  const pStyle = {
+    cursor: 'pointer',
+  };
+
   return (
     <div className="Person">
-      <p onClick={props.click}>
+      <p onClick={props.click} style={pStyle}>
         I'm {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name}/>
+      <input type="text" onChange={props.changed} value={props.name} />
     </div>
   );
 };
