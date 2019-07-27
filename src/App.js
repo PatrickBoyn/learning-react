8ba7ee631feea7e7a85812c6c1d17707f4111b78
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-27 15:45:19 -0400

    Imported the classes by themselves.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -96,7 +96,7 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hello world from React!</h1>
         <p className={classes.join(' ')}>This is really working!</p>
         {/* This can be inefficient. */}
